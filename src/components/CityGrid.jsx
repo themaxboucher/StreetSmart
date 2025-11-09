32fcf9279f0a508b9f3984bce390fe6f70f4893c
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2025-11-08 17:14:10 -0700

    fix bug due to city names being blocked by sliding panel

--- src/components/CityGrid.jsx
+++ src/components/CityGrid.jsx
@@ -71,10 +71,13 @@ export default function CityGrid({onCitySelected}) {
         >
         {/* Overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent" />
-            <div className="absolute bottom-0 left-0 p-3 text-white">
-                <h3 className="text-xl font-bold">{city.name}</h3>
-                <p className="text-sm text-gray-200">{city.country}</p>
-            </div>
+            <div
+  className="absolute bottom-0 left-0 p-3 text-white transform group-hover:-translate-y-35 transition-transform duration-300"
+>
+  <h3 className="text-xl font-bold">{city.name}</h3>
+  <p className="text-sm text-gray-200">{city.country}</p>
+</div>
+
         </div>
         {/* Sliding Panel */}
           <CardContent className="absolute bottom-0 left-0 
